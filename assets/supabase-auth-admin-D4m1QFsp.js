2e5ee731b5c63d37daca434ce8af36a415f1888b
Gokul <112764142+Gokulv21@users.noreply.github.com>
2026-09-13 21:58:44 +0900

    Updates

---
+++ assets/supabase-auth-admin-D4m1QFsp.js
@@ -0,0 +1,1 @@
+import{aO as e}from"./index-BybrVNyp.js";const t=e("https://yyaawwmgzqymyewdmbtj.supabase.co","sb_publishable_kr1nCWS2swThupcIePG9Jg_Ve3_UEfc",{auth:{persistSession:!1,autoRefreshToken:!1,detectSessionInUrl:!1,storage:{getItem:()=>null,setItem:()=>{},removeItem:()=>{}}}});export{t as r};
